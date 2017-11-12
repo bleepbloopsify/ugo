54c2cdb25d4f367e02dc6a098d1c2277684827d8
Leon Chou <bleepbloopsify@gmail.com>
2017-11-12 00:03:25 -0500

    reformatted to fit go language styling protocol

--- test/src/variable_assignment/variable_assignment.go
+++ test/src/variable_assignment/variable_assignment.go
@@ -1,22 +1,24 @@
 package main
+
 import (
-    "fmt"
+	"fmt"
 )
-func returns_two() (int, int) {
-    return 2, 3
+
+func returnsTwo() (int, int) {
+	return 2, 3
 }
 
-func prints_one_of_them(integer int) {
-    fmt.Println(integer)
+func printsOneOfThem(integer int) {
+	fmt.Println(integer)
 }
 
 func main() {
-    integer_1, integer_2 := returns_two()
+	integer1, integer2 := returnsTwo()
 
-    prints_one_of_them(integer_1)
-    prints_one_of_them(integer_2)
-    fmt.Println(integer_1)
+	printsOneOfThem(integer1)
+	printsOneOfThem(integer2)
+	fmt.Println(integer1)
 
-    _, integer_3 := returns_two()
-    fmt.Println(integer_3)
+	_, integer3 := returnsTwo()
+	fmt.Println(integer3)
 }
